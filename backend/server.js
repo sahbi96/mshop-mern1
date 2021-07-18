182fcf05aa453dc318c3c1614c1415c2bbb397fa
sahbi96 <sahbinour@gmail.com>
2021-07-18 22:52:05 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ import mongoose from 'mongoose';
 import UserRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import dotenv from 'dotenv';
+import orderRouter from './routers/orderRouter.js';
+import path from 'path';
+import uploadRouter from './routers/uploadRouter.js';
 
 dotenv.config()
 
@@ -17,9 +20,17 @@ mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost/mshop',{
 })
 
 
-
+app.use('/api/uploads', uploadRouter);
 app.use('/api/users',UserRouter)
 app.use('/api/products',productRouter)
+app.use('/api/orders', orderRouter)
+
+app.get('/api/config/mastercard' , (req,res)=>{
+  res.send(process.env.MASTERCARD_CLIENT_ID || 'sb ')
+})
+
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.get('/',(req,res)=>{
   res.send('Server is OK')
